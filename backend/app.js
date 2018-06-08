434392bf969e3bd6b9981f1a7384e8c66f9b68af
RashikAnsar <sk.ansar46@gmail.com>
2018-06-09 00:21:59 +0530

    Authentication added

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,11 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const postsRoutes = require('./routes/posts');
+const userRoutes = require('./routes/user');
 
 
 const app = express();
-mongoose.connect("mongodb+srv://rashik:IQ9Qmlcu5wjD81A4@cluster0-os7g1.mongodb.net/node-angular?retryWrites=true")
+mongoose.connect("mongodb+srv://rashik:IQ9Qmlcu5wjD81A4@cluster0-os7g1.mongodb.net/node-angular")
   .then(() => {
     console.log('Connected to Database');
   }).catch((err) => {
@@ -21,11 +22,12 @@ app.use('/images', express.static(path.join('backend/images')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
   next();
 });
 
 app.use("/api/posts", postsRoutes);
+app.use("/api/user", userRoutes);
 
 module.exports = app;
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -2,6 +2,7 @@ const express = require('express');
 const multer = require('multer')
 const router = express.Router();
 const Post = require('../models/post');
+const checkAuth = require('../middleware/check-auth');
 
 const MIME_TYPE_MAP = {
   'image/png' : 'png',
@@ -25,7 +26,7 @@ const storage = multer.diskStorage({
   }
 });
 
-router.post('', multer({storage: storage}).single('image') ,(req, res, next) => {
+router.post('', checkAuth,multer({storage: storage}).single('image') ,(req, res, next) => {
   const url = req.protocol + '://' + req.get('host');
   const post = new Post({
     title: req.body.title,
@@ -43,7 +44,7 @@ router.post('', multer({storage: storage}).single('image') ,(req, res, next) => 
   });
 });
 
-router.put('/:id', multer({storage: storage}).single('image'), (req, res, next) => {
+router.put('/:id', checkAuth, multer({storage: storage}).single('image'), (req, res, next) => {
   let imagePath = req.body.imagePath;
   if (req.file) {
     const url = req.protocol + '://' + req.get('host');
@@ -92,7 +93,7 @@ router.get('/:id', (req, res, next) => {
   });
 });
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', checkAuth, (req, res, next) => {
   Post.deleteOne({_id: req.params.id}).then((result) => {
     res.status(200).json({message: 'Post deleted'});
   });
